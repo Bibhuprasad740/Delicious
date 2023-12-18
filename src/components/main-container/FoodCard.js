e63bd7c97c9675d93748ad1d14b648149c67a170
Bibhu Prasad Sahoo <19btcse09@suiit.ac.in>
2023-12-18 14:00:34 +0530

    welcome part complete

---
+++ src/components/main-container/FoodCard.js
@@ -0,0 +1,26 @@
+import React from "react";
+import IcecreamImage from "../../assets/images/i1.png";
+
+const FoodCard = (props) => {
+  return (
+    <div className="lg:w-190 p-4 bg-cardOverlay backdrop-blur-md rounded-3xl flex flex-col items-center justify-center drop-shadow-lg">
+      {/* image */}
+      <img src={props.src} alt="I1" className="w-20 lg:w-40 -mt-10 lg:-mt-20" />
+      {/* name */}
+      <p className="text-base lg:text-xl font-semibold text-textColor mt-2 lg:mt-4">
+        {props.name}
+      </p>
+      {/* description */}
+      <p className="text-sm text-center lg:text-sm text-lighttextGray font-semibold my-1 lg:my-3 ">
+        {props.description}
+      </p>
+      {/* price */}
+      <p className="text-sm font-semibold text-headingColor">
+        <span className="text-xs text-red-600">₹</span>
+        {props.price}
+      </p>
+    </div>
+  );
+};
+
+export default FoodCard;
--- src/components/main-container/ImageSection.js
+++ src/components/main-container/ImageSection.js
@@ -1,5 +1,42 @@
 import React from "react";
 import BackgroundImage from "../../assets/images/heroBg.png";
+import FoodCard from "./FoodCard";
+
+import Icecream from "../../assets/images/i1.png";
+import Strawberry from "../../assets/images/f1.png";
+import Chicken from "../../assets/images/c1.png";
+import Fish from "../../assets/images/fi1.png";
+
+const DUMMY_FOOD_DATA = [
+  {
+    id: 1,
+    name: "Icecream",
+    description: "Chocolate & Vanilla",
+    price: 200,
+    imageSrc: Icecream,
+  },
+  {
+    id: 2,
+    name: "Strawberries",
+    description: "Fresh Strawberries",
+    price: 80,
+    imageSrc: Strawberry,
+  },
+  {
+    id: 3,
+    name: "Chicken",
+    description: "Mixed Chicken Kabab",
+    price: 100,
+    imageSrc: Chicken,
+  },
+  {
+    id: 4,
+    name: "Fish",
+    description: "Fish Kabab",
+    price: 120,
+    imageSrc: Fish,
+  },
+];
 
 const ImageSection = () => {
   return (
@@ -12,6 +49,17 @@ const ImageSection = () => {
       />
 
       {/* food cards */}
+      <div className="w-full h-full absolute top-0 left-0 flex items-center justify-center py-4 lg:px-32 flex-wrap gap-4">
+        {DUMMY_FOOD_DATA.map((food) => (
+          <FoodCard
+            id={food.id}
+            src={food.imageSrc}
+            name={food.name}
+            description={food.description}
+            price={food.price}
+          />
+        ))}
+      </div>
     </div>
   );
 };
